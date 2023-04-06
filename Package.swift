4b6f78ef2a76d87313388de8ba1ab7b3c128b9ba
dasol02 <leedasol2169@gmail.com>
2023-04-06 14:48:32 +0900

    # update DKCamera URL

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
             targets: ["DKImagePickerController"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/zhangao0086/DKCamera", .branch("master")),
+        .package(url: "https://github.com/dslee02/DKCamera.git", .branch("develop")),
         .package(url: "https://github.com/zhangao0086/DKPhotoGallery", .branch("master")),
         .package(url: "https://github.com/TimOliver/TOCropViewController", from: "2.6.0"),
     ],
